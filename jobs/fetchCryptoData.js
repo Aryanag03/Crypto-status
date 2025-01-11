cf564123f02df68fd228025d40e43d2e82ad28db
Aryan Agarwal <121565767+Aryanag03@users.noreply.github.com>
2025-01-11 14:14:46 +0530

    Create fetchCryptoData.js

---
+++ jobs/fetchCryptoData.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+const Crypto = require('../models/Crypto');
+
+const COINS = ['bitcoin', 'matic-network', 'ethereum'];
+const API_URL = 'https://api.coingecko.com/api/v3/simple/price';
+
+const fetchCryptoData = async () => {
+    try {
+        const { data } = await axios.get(API_URL, {
+            params: {
+                ids: COINS.join(','),
+                vs_currencies: 'usd',
+                include_market_cap: true,
+                include_24hr_change: true,
+            },
+        });
+
+        for (const coin of COINS) {
+            const cryptoData = {
+                coin,
+                price: data[coin].usd,
+                marketCap: data[coin].usd_market_cap,
+                change24h: data[coin].usd_24h_change,
+            };
+            await Crypto.create(cryptoData);
+        }
+        console.log('Crypto data fetched and stored successfully.');
+    } catch (error) {
+        console.error('Error fetching crypto data:', error.message);
+    }
+};
+
+module.exports = fetchCryptoData;
